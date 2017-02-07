7d83a1ae5a42d8e99014c8d62a9ad545c56fbeb5
sun <sunrui@wecaresoft.net>
2017-02-07 11:14:51 +0800

    no message

---
+++ insure/insure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  insure
+//
+//  Created by sun on 2017/2/7.
+//  Copyright © 2017年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
